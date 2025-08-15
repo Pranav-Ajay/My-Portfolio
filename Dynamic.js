3acabb1b7a8f92074668d05b6f7d2d3bff74c29d
Pranav-Ajay <pranavajay005@gmail.com>
2025-08-15 20:13:43 +0530

    Update Dynamic.js

--- Dynamic.js
+++ Dynamic.js
@@ -1,6 +1,14 @@
-fetch("https://dev.to/api/articles?per_page=5")
+def action():
+ {fetch("https://dev.to/api/articles?per_page=5")
   .then(response => response.json())
   .then(data => {
     console.log(data);
   });
+  }
   
+btn=document.createlement("Button")
+btn.textcontext="Retrieve"
+btn.addEventListener("click", action())
+
+
+
